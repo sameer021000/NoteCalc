fbb6ccf9d6a5172ef16050c1a134f44aeac2b51d
Sameer Shaik <sameer021000@gmail.com>
2026-08-26 14:18:03 +0530

    Sovled a warning in build.gradle.kts(:app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation(libs.mpandroidchart)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
