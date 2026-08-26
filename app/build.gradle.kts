a8d073a8dd9d88f4c0fc0f8690b32a2fb78f945c
Sameer Shaik <sameer021000@gmail.com>
2026-08-26 13:53:39 +0530

    Updated the version in build.gradle.kts(:app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.notecalc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
